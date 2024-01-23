2c5d9a41284084dc01d5cfcf995a917d9fc50256
Akash Kurdekar <akashkurdekar39@gmail.com>
2024-01-23 20:45:46 +0530

    solving 16, 17, 18

--- step 1 - learn the basics/1.2-patterns/patterns.cpp
+++ step 1 - learn the basics/1.2-patterns/patterns.cpp
@@ -302,10 +302,65 @@ void pattern16(int n)
         cout << endl;
     }
 }
+void pattern16_1(int n)
+{
+    // 1 2 3 4 5
+    // 2 3 4 5
+    // 3 4 5
+    // 4 5
+    // 5
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n;)
+        {
+            j++;
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
 void pattern17(int n)
 {
-}
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < n - i - 1; k++)
+        {
+            cout << "-";
+        }
 
+        char ch = 'A';
+        int bp = (2 * i + 1) / 2;
+        for (int j = 1; j <= 2 * i + 1; j++)
+        {
+            cout << ch;
+            if (j <= bp)
+            {
+                ch++;
+            }
+            else
+            {
+                ch--;
+            }
+        }
+        for (int k = 0; k < n - i - 1; k++)
+        {
+            cout << "-";
+        }
+        cout << endl;
+    }
+}
+void pattern18(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     int t = 1;
@@ -318,7 +373,7 @@ int main()
         // cout << "value of N: ";
         // cin >> n;
 
-        pattern17(n);
+        pattern18(n);
     }
     return 0;
 }
--- step 1 - learn the basics/1.2-patterns/tempCodeRunnerFile.cpp
+++ step 1 - learn the basics/1.2-patterns/tempCodeRunnerFile.cpp
@@ -1,2 +1,6 @@
+r (int j = 0; j <= i; j++)
+        {
+            cout << ch;
+            ch++;
 
-        for (int j = 1; j <= 
+        }
