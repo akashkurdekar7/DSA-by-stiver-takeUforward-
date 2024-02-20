d6b4687c8dee5c0a0dc0a5cbf2b3ffad7ff0d01b
Akash Kurdekar <akashkurdekar39@gmail.com>
2024-02-20 23:45:46 +0530

    some more algorithms

--- step 1 - learn the basics/1.5-learn basic recursion/all.cpp
+++ step 1 - learn the basics/1.5-learn basic recursion/all.cpp
@@ -4,13 +4,61 @@ void recursion(int i, int n)
 {
     if (i < n)
         return;
-    int sum = 0;
     cout << "hiii" << endl;
-    recursion(i + 1, n);
+    recursion(i - 1, n);
+}
+void sum_of_N(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    cout << sum;
+}
+void factorial(int n)
+{
+    int ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        ans *= i;
+    }
+    cout << ans;
+}
+void reverse_array(int n)
+{
+    int a[n] = {1, 2, 3, 4};
+    for (int i = 0; i < n / 2; i++)
+    {
+
+        int temp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = temp;
+    }
+    // Print the reversed array
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+void palindrome(string s)
+{
+    int i = 1, j = s.length();
+    // for (int i = 0; i < s.length(); i++)
+    while (i < j)
+    {
+        
+    }
 }
 int main()
 {
-    int n = 3;
-    recursion(1, n);
+    int n = 5;
+    string s = "ABC";
+    // recursion(n, 1);
+    // sum_of_N(n);
+    // factorial(n);
+    // reverse_array(n);
+    palindrome(s);
     return 0;
 }
