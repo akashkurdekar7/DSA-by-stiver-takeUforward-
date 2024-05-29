ebdf5b69367ff329d2cd09a2aba1b599a912f08f
Akash Kurdekar <akashkurdekar39@gmail.com>
2024-05-30 00:43:15 +0530

    done lec 2.1

---
+++ step 2 - learn the important sorting techniques/lec 1 - Sorting-1/Sorting_Techniques.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class Sorting_Techniques {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+        // selectionSort(arr, n);
+        // bubbleSort(arr, n);
+        insertionSort(arr, n);
+        output(arr, n);
+    }
+
+    // output funtion
+    static void output(int[] arr, int n) {
+        System.out.println("Sorted Elements: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    // selection sort
+    // static void selectionSort(int[] arr, int n) {
+    // for (int i = 0; i < n; i++) {
+    // for (int j = 0; j < n; j++) {
+    // if (arr[i] < arr[j]) {
+    // int temp = arr[i];
+    // arr[i] = arr[j];
+    // arr[j] = temp;
+    // }
+    // }
+    // }
+    // }
+
+    // bubble sort
+    // static void bubbleSort(int[] arr, int n) {
+    // for (int i = n - 1; i >= 0; i--) {
+    // for (int j = 0; j <= i - 1; j++) {
+    // if (arr[j] > arr[j + 1]) {
+    // int temp = arr[j + 1];
+    // arr[j + 1] = arr[j];
+    // arr[j] = temp;
+    // }
+    // }
+    // }
+    // }
+
+    // insertion sort
+    static void insertionSort(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+}
