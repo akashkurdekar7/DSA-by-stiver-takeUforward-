52b6aa4d7143f9c35fcb8517407a7b76d7f88b9a
Akash Kurdekar <akashkurdekar39@gmail.com>
2024-05-30 00:28:37 +0530

    .

--- step 2 - learn the important sorting techniques/lec 1 - Sorting-1/Sorting_Techniques.cpp
+++ step 2 - learn the important sorting techniques/lec 1 - Sorting-1/Sorting_Techniques.cpp
@@ -35,16 +35,15 @@ void bubbleSort(int a[], int n)
 
 void InsertionSort(int a[], int n)
 {
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= i - 1; j++)
+        int j = i;
+        while (j > 0 && a[j - 1] > a[j])
         {
-            if (a[j] > a[j + 1])
-            {
-                int temp = a[j + 1];
-                a[j + 1] = a[j];
-                a[j] = temp;
-            }
+            int temp = a[j - 1];
+            a[j - 1] = a[j];
+            a[j] = temp;
+            j--;
         }
     }
 }
@@ -69,8 +68,8 @@ int main()
         cin >> a[i];
     }
 
-    selectionSort(a, n);
-    bubbleSort(a, n);
+    // selectionSort(a, n);
+    // bubbleSort(a, n);
     InsertionSort(a, n);
     output(a, n);
     return 0;
