74121b37186969c6485823d31466940cbfd6ef06
Akash Kurdekar <akashkurdekar39@gmail.com>
2024-06-14 22:09:30 +0530

    .

--- step 3 - array problems {EMH}/easy/array_easy_problems.cpp
+++ step 3 - array problems {EMH}/easy/array_easy_problems.cpp
@@ -36,10 +36,59 @@ void Second_largest_lowest_element(vector<int> &arr, int n)
         cout << "provide more elements";
         return;
     }
-    int small = __INT_MAX__;
-    int second_small = __INT_MAX__;
-    int highest = __INT_MAX__;
-    int second_highest = __INT_MAX__;
+    int small = INT_MAX;
+    int second_small = INT_MAX;
+    int highest = INT_MIN;
+    int second_highest = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        small = min(small, arr[i]);
+        highest = max(highest, arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < second_small && arr[i] != small)
+        {
+            second_small = arr[i];
+        }
+        if (arr[i] > second_highest && arr[i] != highest)
+        {
+            second_highest = arr[i];
+        }
+    }
+    cout << "Second smallest is " << second_small << endl;
+    cout << "Second largest is " << second_highest << endl;
+}
+
+void sort(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j <= n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+bool check_sorted(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 int main()
@@ -54,7 +103,13 @@ int main()
         cin >> arr[i];
     }
     // largest_lowest_element(arr, n);
-    Second_largest_lowest_element(arr, n);
+    // Second_largest_lowest_element(arr, n);
+    bool ans = check_sorted(arr, n);
+    if (ans)
+        cout << "true" << endl;
+    else
+        cout << "false" << endl;
+    // sort(arr, n);
     output(arr, n);
     return 0;
 }
